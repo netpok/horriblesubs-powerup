bceb036099b00d5668262f4cff3c07359348add4
netpok <netpok.reg@gmail.com>
2016-10-14 20:34:48 +0200

    Add gitignore and script file with headers

---
+++ hspu.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         HorribleSubs PowerUP
+// @namespace    https://github.com/netpok/horriblesubs-powerup
+// @version      0.1
+// @description  Add AniDB links, update times and removes JLink
+// @author       netpok
+// @match        http://horriblesubs.info/*
+// @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.5.6/moment-timezone-with-data.min.js
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+(function () {
+
+})();
